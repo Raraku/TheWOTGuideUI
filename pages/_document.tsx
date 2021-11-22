c0123293bbd8f9487fced8518511eb9ef21e497c
raraku <airenov500@gmail.com>
2021-11-23 00:40:37 +0100

    wash

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,35 @@
-
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { resolve } from 'url'
 import { processEnv } from '@lib/processEnv'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
-
   static async getInitialProps(ctx: DocumentContext) {
     return await super.getInitialProps(ctx)
   }
 
   render() {
     const { pageProps } = this.props.__NEXT_DATA__.props
-    const { cmsData, settings }  = pageProps || { cmsData: null, settings: null }
-    const { settings: cmsSettings , bodyClass } = cmsData || { settings: null, bodyClass: '' }
+    const { cmsData, settings } = pageProps || { cmsData: null, settings: null }
+    const { settings: cmsSettings, bodyClass } = cmsData || { settings: null, bodyClass: '' }
     const { lang } = settings || cmsSettings || { lang: 'en' }
 
     return (
-      <Html {...{lang, className: 'casper' }}>
+      <Html {...{ lang, className: 'casper' }}>
         <Head>
-          <link
-            rel="alternate"
-            type="application/rss+xml"
-            title="Jamify RSS Feed"
-            href={`${resolve(processEnv.siteUrl, 'rss.xml')}`}
-          />
-           <script async src="https://www.googletagmanager.com/gtag/js?id=G-6BQFHE8J0J"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          <link rel="alternate" type="application/rss+xml" title="Jamify RSS Feed" href={`${resolve(processEnv.siteUrl, 'rss.xml')}`} />
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-6BQFHE8J0J" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-6BQFHE8J0J');`,
-            }}
-          />
+  gtag('config', 'G-6BQFHE8J0J');
+            `}
+          </Script>
         </Head>
-        <body {...{className: bodyClass}}>
+        <body {...{ className: bodyClass }}>
           <script
             dangerouslySetInnerHTML={{
               __html: `
